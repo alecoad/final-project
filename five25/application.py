ab6b5a62eeb9d80cc17dcf75f1edcc49d3d334cf
alecoad <andrewlecoad@gmail.com>
2020-05-09 10:55:20 -0600

    Add create view

--- five25/application.py
+++ five25/application.py
@@ -8,6 +8,18 @@ goals = []
 def index():
     return render_template('index.html', goals=goals)
 
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.html')
+    else:
+        for i in range(3):
+            goal = request.form.get('goal' + str(i))
+            goals.append(goal)
+        return redirect('/')
+
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'GET':
